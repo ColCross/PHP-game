eac451591fa84b09fc54fd027fc70bbec3f958de
RyHutch <rhutchins42@gmail.com>
2017-03-05 17:08:53 -0500

    Fixed Some Bugs

--- dicegame2.php
+++ dicegame2.php
@@ -1,51 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Project 2</title>
-
-
+    <title>Project 2</title>
 </head>
 
 <body>
 
+<form action="dicegame3.php" method="post">
 
-	<form action="dicegame3.php" method="post">
-	
-	<h1>Players</h1>
-	<select name="players">
-		<option value="2">two</option>
-		<option value="3">three</option>
-		<option value="4">four</option>
-		<option value="5">five</option>
-		<option value="6">six</option>
-	</select>
-	
-	
-	<h1>Enter Number of Dice</h1>
-	<select name="dice">
-		<option value="3">Three</option>
-		<option value="4">Four</option>
-		<option value="5">five</option>
-		<option value="6">six</option>
-		<option value="7">seven</option>
-		<option value="8">eight</option>
-	</select>
-	
-	<br></br>
-	<input type = "submit" value = "Click" name = "submit"/>
+    <h1>Players</h1>
+    <select name="players">
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+        <option value="4">Four</option>
+        <option value="5">Five</option>
+        <option value="6">Six</option>
+    </select>
+
+    <h1>Enter Number of Dice</h1>
+
+    <select name="dice">
+        <option value="3">Three</option>
+        <option value="4">Four</option>
+        <option value="5">Five</option>
+        <option value="6">Six</option>
+        <option value="7">Seven</option>
+        <option value="8">Eight</option>
+    </select>
+
+    <br></br>
+    <input type="submit" value="Click" name="submit"/>
 
 </form>
 
 <?php
-	session_start();
-	
-
-	
+session_start();
 ?>
-
-
-
-
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,51 +5,60 @@
 </head>
 
 <body>
-    <h1>
-        <?php
-        echo"Liar's Dice"
-        ?>
-    </h1>
-    <h2>
-        <?php
-        echo "How to play: Place a bet on how many of the die you think <br>is in the game including your own and 
-                the other players'. <br>For example: I bet that there are 4 2's. <br>Depending on whose turn it is,
-                the player may decide <br>to bid or call bluff. If a player guesses correctly, each player loses<br>
-                a die. Last one standing will be the winner."
-        ?>
-    </h2>
-    <form action="game.php" method="POST">        
-        Number of Players:
-        <select class="numPlayers">
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-        </select>
-        <br />
-        Number of Dice:
-        <select class="numDice">
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-            <option>9</option>
-            <option>10</option>
-        </select>
-        <br />
-        <input type="submit" value="Play" onclick="window.location.href='index.php'" />
-    </form>
 
-     <?php
-     $numPlayers = 2;
-     $numDice = 3;
-     if(isset($_POST['submit'])){
-         $numPlayers = $_POST["numPlayers"];
-         $numDice = $_POST["numDice"];
-     }
-     
-     ?>
+<!--    <h1>-->
+<!--        --><?php
+//        echo"Liar's Dice"
+//        ?>
+<!--    </h1>-->
+<!--    <h2>-->
+<!--        --><?php
+//        echo "How to play: Place a bet on how many of the die you think <br>is in the game including your own and
+//                the other players'. <br>For example: I bet that there are 4 2's. <br>Depending on whose turn it is,
+//                the player may decide <br>to bid or call bluff. If a player guesses correctly, each player loses<br>
+//                a die. Last one standing will be the winner."
+//        ?>
+<!--    </h2>-->
+<!--    <form action="game.php" method="POST">        -->
+<!--        Number of Players:-->
+<!--        <select class="numPlayers">-->
+<!--            <option>2</option>-->
+<!--            <option>3</option>-->
+<!--            <option>4</option>-->
+<!--        </select>-->
+<!--        <br />-->
+<!--        Number of Dice:-->
+<!--        <select class="numDice">-->
+<!--            <option>3</option>-->
+<!--            <option>4</option>-->
+<!--            <option>5</option>-->
+<!--            <option>6</option>-->
+<!--            <option>7</option>-->
+<!--            <option>8</option>-->
+<!--            <option>9</option>-->
+<!--            <option>10</option>-->
+<!--        </select>-->
+<!--        <br />-->
+<!--        <input type="submit" value="Play" onclick="window.location.href='index.php'" />-->
+<!--    </form>-->
+<!---->
+<!--     --><?php
+//     $numPlayers = 2;
+//     $numDice = 3;
+//     if(isset($_POST['submit'])){
+//         $numPlayers = $_POST["numPlayers"];
+//         $numDice = $_POST["numDice"];
+//     }
+//
+//     ?>
+
+<?php
+    $test = array(0, 1, 2);
+    echo implode($test);
+    echo "<br>";
+    $test = 1;
+    echo "$test";
+?>
 
 </body>
 </html>
